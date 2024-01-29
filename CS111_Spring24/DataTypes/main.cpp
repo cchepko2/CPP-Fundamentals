50681393e3672006b52d41a2c8f892d8ab1e859f
Corin Chepko <cchepko2@coloradomesa.edu>
2024-01-29 15:27:38 -0700

    sg

---
+++ CS111_Spring24/DataTypes/main.cpp
@@ -0,0 +1,17 @@
+/*
+Corin Chepko
+1-29-24
+Examples of sizeof() function and data types
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Int size: " << sizeof(int) << endl;
+    cout << "Float size: " << sizeof(float) << endl;
+    cout << "Double size: " << sizeof(double) << endl;
+
+    return 0;
+}
