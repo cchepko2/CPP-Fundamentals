d4c174eca9ecf5f4b4f81152082d22886cae0427
Corin Chepko <cchepko2@coloradomesa.edu>
2024-01-29 15:51:10 -0700

    sdg

--- CS111_Spring24/DataTypes/main.cpp
+++ CS111_Spring24/DataTypes/main.cpp
@@ -4,6 +4,7 @@ Corin Chepko
 Examples of sizeof() function and data types
 */
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main(void)
     cout << "Int size: " << sizeof(int) << endl;
     cout << "Float size: " << sizeof(float) << endl;
     cout << "Double size: " << sizeof(double) << endl;
-
+    cout << "2^8 - 1: " << pow(2,8) << endl;
+    cout << 15*16+8 << endl;
     return 0;
 }
