9505bf7260f7b98ab6decdd4bcc3d5878fdefab1
Corin Chepko <cchepko2@coloradomesa.edu>
2024-03-13 15:51:01 -0600

    awr

--- CS111_Spring24/arrays/values_memory.cpp
+++ CS111_Spring24/arrays/values_memory.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main(void)
 {
     char name[] = "Corin";
+    char *name2 = name;
     int num1 = 1;
     //char letter = 'C';
     float letter = 67;
     double big_num = 55834.24;
 
-    cout << "name at " << &name[0] << endl;
+    //cout << "name at " << &name[0] << endl;
+    cout << name << endl;
+    name[2] = 'V';
+    cout << name2 << endl;
+    printf("name at %x\n", name);
     cout << "num1 at " << &num1 << endl;
     cout << "letter at " << &letter << endl;
     cout << "big_num at " << &big_num << endl;
@@ -18,8 +23,6 @@ int main(void)
     cout << "double is "<< sizeof(double)<<endl;
     cout << "float is "<< sizeof(float)<<endl;
     cout << "int is "<< sizeof(int)<<endl;
-
-    cout << "Happy birthday Corin!" << endl;
 
     return 0;
 }
