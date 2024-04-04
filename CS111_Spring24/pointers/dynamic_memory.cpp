71abae1fa53efcfbe7dcf0fb0d147fce7ecb1722
Corin Chepko <cchepko2@coloradomesa.edu>
2024-04-04 15:10:43 -0600

    pointer stuff

--- CS111_Spring24/pointers/dynamic_memory.cpp
+++ CS111_Spring24/pointers/dynamic_memory.cpp
@@ -19,9 +19,9 @@ int main(void)
     std::cin >> rows >> columns;
 
     double *distances = new double[rows*columns];
-    double *speeds = new double[columns];
-    double *angles = new double[rows];
-    for(int i=0; i<columns; i++) // Fill in speeds array
+    double *speeds = new double[columns+1];
+    double *angles = new double[rows+1];
+    for(int i=0; i<=columns; i++) // Fill in speeds array
     {
         double speed_inc = 100.0/columns;
         *(speeds+i) = i*speed_inc;
@@ -29,24 +29,26 @@ int main(void)
         std::cout << *(speeds+i) << ',';
     }
     fout << '\n';
-    for(int i=0; i<rows; i++) // Fill in angles array
+    std::cout << '\n';
+    for(int i=0; i<=rows; i++) // Fill in angles array
     {
         double angle_inc = 90.0/rows;
         *(angles+i) = i*angle_inc;
     }
 
-    std::cout << distances << std::endl;
+    //std::cout << distances << std::endl;
 
     for(int i=0; i<rows; i++)
     {
-        fout << *(angles + i) << ',';
+        fout << *(angles + i+1) << ',';
+        std::cout << *(angles + i+1) << ',';
         for(int j=0; j<columns; j++)
         {
-            *(distances + i*columns+j) = i*columns+j;
+            double val = *(distances + i*columns+j) = i*columns+j;
             //double *address_of_current_val;
             //address_current_val = distances;
-            std::cout << *(distances + i*columns+j) << ',';
-            fout << *(distances + i*columns+j) << ',';
+            std::cout << val << ',';
+            fout << val << ',';
         }
         std::cout << '\n';
         fout << '\n';
