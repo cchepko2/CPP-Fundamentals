8845a3ab004604ae8f840557883cdc63a8d9bc9e
Corin Chepko <cchepko2@coloradomesa.edu>
2024-04-30 15:49:47 -0600

    words

--- CS111_Spring24/Examples/words_in.cpp
+++ CS111_Spring24/Examples/words_in.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
-    
+    ifstream fin;
+    string line;
+    //char *words[] = new char[235886];
+    vector<string> words;
+    char word[100];
+
+    fin.open("dict-words.txt");
+
+    if(!fin.is_open())
+    {
+        cout << "Unable to load dictionary!" << endl;
+        return -1;
+    }
+
+    int lines=0;
+    while( getline(fin, line) )
+    {
+        //strncpy(word,line.c_str(), 99);
+        words.push_back(line);
+        lines++;
+        //cout << line << endl;
+    }
+
+    srand(time(NULL));
+
+    int random_word = rand()%lines;
+
+    cout << words[random_word] << endl;
+
+
+    fin.close();
 
     return 0;
 }
