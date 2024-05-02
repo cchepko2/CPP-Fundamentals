1920b2f1ae2bafd7fc705e305746b756a116dc2c
Corin Chepko <cchepko2@coloradomesa.edu>
2024-05-02 15:07:45 -0600

    clear_scn example

---
+++ CS111_Spring24/Examples/clear_scr.cpp
@@ -0,0 +1,10 @@
+// function clears the screen using system call
+// NOTE: system call is not a security best pracice!
+void clearScreen() {
+    // use "cls" in windows and "clear" command in Mac and Linux
+    #ifdef _WIN32
+        system("clS");
+    #else
+        system("clear");
+    #endif
+}
