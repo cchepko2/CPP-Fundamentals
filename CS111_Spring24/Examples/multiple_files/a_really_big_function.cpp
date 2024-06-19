07ea95389a92560ac839f827c27be8084410f16e
Corin Chepko <cchepko@gmail.com>
2024-06-19 07:09:23 -0600

    Removed previous semester solutions and examples

--- CS111_Spring24/Examples/multiple_files/a_really_big_function.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void a_really_big_function()
-{
-    cout << "Hello world!" << endl;
-}
--- CS111_Spring24/Examples/multiple_files/a_really_big_function.h
+++
@@ -1,7 +0,0 @@
-void a_really_big_function();
-
-const std::string scissors = R"(  _       ,/'
-  (_).  ,/'
-   _  ::
-  (_)'  `\.
-           `\.)";
