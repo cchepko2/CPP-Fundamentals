2b2a811967de61c8a7ed29ffa28aa3d8c94dbe32
Corin Chepko <cchepko2@coloradomesa.edu>
2024-09-30 14:40:48 -0600

    xv

---
+++ CS130_Fall24/Project1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+double GRAVITY = 9.81;
+
+int main(void)
+{
+    double velocity, angle, target_dist;
+    double max_height, distance, flight_time;
+
+    cout << "Welcome to my program.\n
+        This program will calculate the distance, height, and flight_time of\n
+        a projectile given it's velocity and angle of launch.\n
+        It will also tell if a target distance is within the maximum distance\n
+        the projectile can fly; given a 45 degree angle" << endl;
+
+    cout << "Please enter the velocity of the projectile: " << endl;
+    
+
+    return 0;
+}
