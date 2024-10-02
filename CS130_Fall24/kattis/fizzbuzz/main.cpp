222873fdc998cba15a3dc91349268d5a7c27b1be
Corin Chepko <cchepko2@coloradomesa.edu>
2024-10-02 15:52:47 -0600

    kattis examples

--- CS130_Fall24/kattis/fizzbuzz/main.cpp
+++ CS130_Fall24/kattis/fizzbuzz/main.cpp
@@ -5,28 +5,69 @@ Program: kattis problem - https://open.kattis.com/problems/fizzbuzz
 Algorithm Steps:
     input integers x, y, and N
     print numbers 1 through N
+        if(number divisible by both) (n%x == 0 and n%y == 0)
+            cout << "FixxBuzz"
         if( number divisible by x) (n%x == 0)
             cout << "Fizz"
         if( number divibile by y)   (n%y == 0)
             cout << "Buzz"
-        if(number divisible by both) (n%x == 0 and n%y == 0)
-            cout << "FixxBuzz"
 */
 
 #include <iostream>
+#include <cassert>
+//#include <string>
 
 using namespace std;
 
-int main(void)
+string fizzbuzz(int i, int x, int y);
+void test();
+
+int main(int argc, char * argv[])
 {
+    if(argc > 1 && string(argv[1]) == "test")
+    {
+        test();
+        return 0;
+    }
+
     int x, y, n;
 
     cin >> x >> y >> n;
 
-    for() // for n times...
+    for(int i=1; i<=n; i++) // for n times...
     {
-        cout << stuff;
+        cout << fizzbuzz(i, x, y) << endl;
     }
 
     return 0;
+}
+
+string fizzbuzz(int i, int x, int y)
+{
+    if(i%x==0 && i%y == 0)
+    {
+        return "FizzBuzz";
+    }
+    else if(i%x == 0)
+    {
+        return "Fizz";
+    }
+    else if(i%y == 0)
+    {
+        return "Buzz";
+    }
+    else
+    {
+        return to_string(i); // converts int i to a c++ string
+    }
+}
+
+void test()
+{
+    assert(fizzbuzz(3, 3, 6) == "Fizz");
+    assert(fizzbuzz(3, 2, 3) == "Buzz");
+    assert(fizzbuzz(6, 3, 6) == "FizzBuzz");
+    assert(fizzbuzz(5, 3, 4) == "5");
+
+    cerr << "All tests passed!" << endl;
 }
