9af975f6ecfd8df88f8088b945b6e9d68eb0bf71
Corin Chepko <cchepko2@coloradomesa.edu>
2025-03-27 16:47:03 -0600

    what does the fox say

--- Spring25Notes/Kattis/whatdoesthefoxsay/main.cpp
+++ Spring25Notes/Kattis/whatdoesthefoxsay/main.cpp
@@ -15,6 +15,7 @@ Algorithm Steps:
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -26,32 +27,53 @@ int main(void)
     string sounds_str;
     string third_word;
 
-    /*cin >> num_tests;
+    cin >> num_tests;
+    cin.ignore(1000, '\n');
     for(int i=0; i<num_tests; i++)
     {
-            // We'll work on this part first
-    }*/
-
-    cin.ignore(1000, '\n');
-    getline(cin, sounds_str);
-    do
-    {
-        cin >> third_word >> third_word >> third_word;
-        if(third_word != "the")
+        getline(cin, sounds_str);
+        istringstream iss(sounds_str);  // Create input string of sounds to unpack into a vector
+        while( iss >> third_word )  // Read each word and put into vector
         {
-            known_sounds.push_back(third_word);
+            sounds.push_back(third_word);
         }
-    } while (third_word != "the");
 
-    cout << sounds_str << endl;
-    for(string sound:known_sounds)
-    {
-        cout << sound << " ";
+        do
+        {
+            cin >> third_word >> third_word >> third_word;
+            if(third_word != "the")
+            {
+                known_sounds.push_back(third_word);
+            }
+            else{
+                cin >> third_word >> third_word; // Get rid of "fox say"
+                cin.ignore(1000, '\n');
+                break;
+            }
+        } while (third_word != "the");
+
+
+        string answer = "";
+        for(string sound:sounds)
+        {
+            bool match = false;
+            for(string animal_sound:known_sounds)
+            {
+                if( sound == animal_sound )
+                {
+                    match = true;
+                }
+            }
+            if(match == false) // This sounds does not match any known sounds
+            {
+                answer += sound + ' ';
+            }
+        }
+        cout << answer << endl;
+        
+        sounds.clear();
+        known_sounds.clear();
     }
-    cout << endl;
-    
-    
-    
 
     return 0;
 }
