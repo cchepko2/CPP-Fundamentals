376e46a556b480d8f70dcf116a7da3d274492431
Corin Chepko <cchepko2@coloradomesa.edu>
2025-04-02 16:56:02 -0600

    Projectile 2

--- Spring25Notes/Project2/main.cpp
+++ Spring25Notes/Project2/main.cpp
@@ -10,9 +10,10 @@ compute the sin and cosine of the angle.
 
 #define drag_coeff 0.47
 #define air_density 1.225
-#define time_step 0.01
+#define time_step 0.0001
 #define radius 0.05
 #define mass 1
+#define GRAVITY -9.81
 
 using namespace std;
 
@@ -22,7 +23,15 @@ int main(void)
 {
     char units;
     double angle, conversion_factor, velocity;
-    double x=0, y=0;
+    double x=0, y=0, time=0, max_height = 0;
+
+    cout << "Enter the velocity: ";
+    cin >> velocity;
+    cout << "Enter the angle in degrees: ";
+    cin >> angle;
+
+    angle = angle*M_PI/180.0;
+
     double v_vert = velocity*sin(angle);
     double v_horz = velocity*cos(angle);
     double area = M_PI*pow(radius, 2);
@@ -31,12 +40,27 @@ int main(void)
     {
         x += v_horz * time_step;
         y += v_vert * time_step;
+        time += time_step;
+
+        velocity = sqrt(pow(v_vert, 2) + pow(v_horz, 2));
+        
+        if( y > max_height)
+            max_height = y;
 
         double accel = air_density*drag_coeff*pow(velocity, 2)*area/(2*mass);
+        double accel_y = accel*v_vert/velocity;
+        double accel_x = accel*v_horz/velocity;
+
+        //cout << accel_x << ' ' << accel_y << endl;
+
+        v_vert += GRAVITY*time_step - accel_y*time_step;
+        v_horz -= accel_x*time_step;
         
     }
 
-
+    cout << "Final time is: " << time << " seconds." << endl;
+    cout << "Maximum height is: " << max_height << " meters." << endl;
+    cout << "Final distance is: " << x << " meters." << endl;
     
     return 0;
 }
