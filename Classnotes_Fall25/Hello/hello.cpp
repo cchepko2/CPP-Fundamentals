234774f7f4ea4ad525e1446c0426f3ff7df89bcb
Corin Chepko <cchepko2@coloradomesa.edu>
2025-08-25 15:53:51 -0600

    dg

--- Classnotes_Fall25/Hello/hello.cpp
+++ Classnotes_Fall25/Hello/hello.cpp
@@ -5,6 +5,7 @@ Description: Hello World Program
 */
 
 #include <iostream>
+#include <stdio.h>
 
 // Include the std namespace so we don't have to type "std::" before every cout statement
 using namespace std;
@@ -14,11 +15,16 @@ int main(void)
     // A type char variable, will take the character corresponding the the ascii number
     // These two lines are the same
     char letter = 'A'+32;
-    int number = 0b1010;
+    //int number = 0b01111111111111111111111111111111;
+    int number = 0b00010110;
+    int neg_num = 0b10000000000000000000000000000000;
+    float f_number = 6.2;
     //char letter = 65;
 
     cout << "The size of char letter is " << sizeof(letter) << " bytes." << endl;
     cout << "The value of number is " << number << endl;
+    cout << "The value of neg number is " << neg_num << endl;
+    printf("f_number = %.8f\n", f_number);
 
     cout << letter << endl;
     cout << "Hello \n World!" << endl;
