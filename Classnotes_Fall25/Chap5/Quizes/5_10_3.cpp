4ccb742a66c0b1231a8d702cf8100b8124a72302
Corin Chepko <cchepko2@coloradomesa.edu>
2025-09-29 15:54:26 -0600

    sge

--- Classnotes_Fall25/Chap5/Quizes/5_10_3.cpp
+++ Classnotes_Fall25/Chap5/Quizes/5_10_3.cpp
@@ -5,15 +5,16 @@ using namespace std;
 int main()
 {
     string in_string = "<windows.h>";
-    int i=0, j=0;
+    int j=0;
     char repeated;
     bool found = false;
 
     cout << "Enter a string: ";
     cin >> in_string;
-
-    for(i=0; i<in_string.size(); i++)
+   
+    for(int i=0; i<in_string.size(); i++)
     {
+        string name = "Sally";
         for(j=i+1; j<in_string.size(); j++)
         {
             if(in_string[i] == in_string[j] and found != true)
@@ -28,6 +29,12 @@ int main()
         cout << "The first repeated character is " << repeated << endl;
     else
         cout << "No repeated character was found" << endl;
+
+
+   if(again)
+   {
+    main();
+   } 
 
     return 0;
 }
