f719570adc915343e79c27d9139be0ab91795cd6
Corin Chepko <cchepko2@coloradomesa.edu>
2026-01-23 16:45:58 -0700

    First Program

---
+++ ClassnotesSpring2026/firstprogram.cpp
@@ -0,0 +1,25 @@
+/*=================================================
+  Hello World program
+  Author: Ram Basnet
+  Date: June 24, 2020
+  Copyright: MIT License
+  
+  The program prints "Hello World!" on the console
+===================================================*/
+
+// include required libraries/header files
+#include <iostream>
+
+// Using standard namespace, means we don't need to type 
+// std:: in front of all our input and output coommands
+using namespace std;
+
+// one main function is always required in a C++ program
+int main() // main entry to the program 
+{
+    // output Hello World!
+    cout << "Hello World\"" << "!";
+    cout << "Hello World!";
+
+    return 0;
+}
