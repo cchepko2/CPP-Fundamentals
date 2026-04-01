3430242b52d7394b7d8114d6bd82257df167cee8
Corin Chepko <cchepko2@coloradomesa.edu>
2026-04-01 16:37:05 -0600

    Euler Sim w air resistance

--- ClassnotesSpring2026/Euler Simulation/euler.cpp
+++ ClassnotesSpring2026/Euler Simulation/euler.cpp
@@ -9,103 +9,88 @@ Structures declaration and use
 using namespace std;
 
 struct PROJECTILE {
+    // Independent variables
      double mass;
      double radius;
+     double drag_coeff;
+
+     // Dependent variables to be calculated
      double distance;
      double time;
      double height;
     };
 
-void get_distance(PROJECTILE &projectile);
+void get_distance_euler(PROJECTILE &projectile, double angle, double velocity);
 
 int main()
 {
-    string name = "Corin";
     PROJECTILE Projectile1; 
 
     Projectile1.mass = 5.0;
     Projectile1.radius = 0.5;
+    Projectile1.drag_coeff = 0.47;
 
     cout << "Mass = " << Projectile1.mass << endl;
     cout << "Radius = " << Projectile1.radius << endl;
 
-    get_distance(Projectile1);
+    double angle;
+    double velocity;
+
+    cout << "Enter an angle in degrees: ";
+    cin >> angle;
+    angle = angle*M_PI/180.0;
+
+    cout << "Enter a velocity in m/s: ";
+    cin >> velocity;
+
+    get_distance_euler(Projectile1, angle, velocity);
 
     cout << "Distance traveled = " << Projectile1.distance << endl;
-    cout << "Height = " << Projectile1.distance << endl;
-    cout << "Flight time = " << Projectile1.distance << endl;
+    cout << "Height = " << Projectile1.height << endl;
+    cout << "Flight time = " << Projectile1.time << endl;
 
 
     return 0;
 }
 
-void get_distance(PROJECTILE &projectile)
+// Euler version of get_distance
+void get_distance_euler(PROJECTILE &projectile, double angle, double velocity)
 {
     double x = 0, y = 0;
-    double angle = 30*M_PI/180.0;
-    double velocity = 50;
     double x_accel, y_accel, x_vel, y_vel;
+    double max_height = 0, time = 0;
+    double curr_velocity, curr_accel_air;
 
     const double gravity = 9.81;
-    const double time_step = 0.1;
-
+    const double time_step = 0.001;
+    const double air_density = 1.225;
+    double area = pow(projectile.radius, 2)*M_PI;
+    
     x_vel = velocity*cos(angle);
     y_vel = velocity*sin(angle);
 
     while(y > -0.01)
     {
+        curr_velocity = sqrt(pow(x_vel, 2) + pow(y_vel, 2));
+        curr_accel_air = 0.5*air_density*projectile.drag_coeff*pow(curr_velocity, 2)*area/projectile.mass;
+
+        x_accel = -1*curr_accel_air*(x_vel/curr_velocity)*time_step;
+        y_accel = -1*curr_accel_air*(y_vel/curr_velocity)*time_step;
          
         x += x_vel*time_step;
         y += y_vel*time_step;
 
-        cout << "x = " << x << "  y = " << y << endl;
+        y_vel += y_accel - gravity*time_step;
+        x_vel += x_accel*time_step;
 
-        y_vel -= gravity*time_step;
-        //x_vel += 0*time_step;
-    }
-
-    cout << "distance = " << x << endl;
-
-}
-
-/*void euler_dist(double angle, double velocity, double &dist, double &time, double &height)
-{
-    const double TIME_INC = 0.001;
-    const double mass = 5;
-    const double radius = 0.5;
-    double area = PI*pow(radius, 2);
-    const double coeff = 0.47;
-    const double air_density = 1.225;
-
-    double x=0, y=0;
-    double x_vel = cos(angle*PI/180.0)*velocity;
-    double y_vel = sin(angle*PI/180)*velocity;
-    time = 0;
-    height = 0;
-    double curr_velocity, curr_accel_air;
-    double x_decel, y_decel;
-
-    while(y >= 0)
-    {
-        curr_velocity = sqrt(pow(x_vel, 2) + pow(y_vel, 2));
-        curr_accel_air = 0.5*air_density*coeff*pow(curr_velocity, 2)*area/mass;
-
-        x_decel = -1*curr_accel_air*(x_vel/curr_velocity)*TIME_INC;
-        y_decel = -1*curr_accel_air*(y_vel/curr_velocity)*TIME_INC;
-
-        y_vel += -1*GRAVITY*TIME_INC + y_decel;
-        x_vel += x_decel;
-
-        x += x_vel*TIME_INC;
-        y += y_vel*TIME_INC;
-
-        
-        time += TIME_INC;
-        if(y > height)
+        if( y>max_height)
         {
-            height = y;
+            max_height = y;
         }
+        time += time_step;
     }
-    dist = x;
-    //cout << curr_velocity << endl;
-}*/
+
+    projectile.distance = x;
+    projectile.time = time;
+    projectile.height = max_height;
+}
