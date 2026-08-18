41127eaa7facc00b6f837e51d92590ee25776abe
Corin Chepko <cchepko2@coloradomesa.edu>
2026-08-18 15:34:26 -0600

    NotesSP26 moved

--- ClassnotesSpring2026/Matlab Notes/Prof Wall Lecture - Week #11 (MATLAB Functions)/kinetic_energy_func.m
+++
@@ -1,9 +0,0 @@
-function energy = kinetic_energy_func(mass, velocity)
-% kinetic_energy_func(mass, velocity)
-%
-% Returns the kinetic energy of a particle whose mass and velocity are passed in.
-%
-
-energy = 0.5 * mass .* (velocity .^ 2);
-
-end
