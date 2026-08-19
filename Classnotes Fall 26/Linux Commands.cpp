198a6f4cfc0a992eaa2361b6bbd0ec680a7df72f
Corin Chepko <cchepko2@coloradomesa.edu>
2026-08-19 15:36:09 -0600

    Linux command

---
+++ Classnotes Fall 26/Linux Commands.cpp
@@ -0,0 +1,13 @@
+// This is a comment
+/*
+Author: Corin Chepko
+Date: 8/19/26
+Program Description: Just linux ommands, this is not a c program
+*/
+
+// First command: ls (stands for list), it lists the contents of a folder
+// ls -a (lists all folders and files inclucing hidden ones)
+
+// cd (stands for change directory)
+// cd <folder name> (moves within the folder name)
+// cd .. (moves up into the parent directory)
