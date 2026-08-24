448d0278aabd2bc6410cbdc836decb5fa1214493
Corin Chepko <cchepko2@coloradomesa.edu>
2026-08-24 15:52:10 -0600

    Linux

--- Classnotes Fall 26/Linux Commands.cpp
+++ Classnotes Fall 26/Linux Commands.cpp
@@ -11,3 +11,17 @@ Program Description: Just linux commands, this is not a c program
 // cd (stands for change directory)
 // cd <folder name> (moves within the folder name)
 // cd .. (moves up into the parent directory)
+
+// mkdir    (creates a directory)
+// rm <filename>
+// rm -r    (rm a directory and all things inside)
+// rm -r -f (rm a directory and everything in it and don't ask me questions)
+// cat      (list contents of a text file)
+// cp <filename> <location>
+// mv <filename or directory> <new filename or directory>
+
+// Git Commands
+    // git add -A    (stages all changes)
+    // git commit -m "A message" (commits changes)
+    // git push     (push changes to GitHub)
+    // git pull     (pulls changes from GitHub if changes were made on another system)
