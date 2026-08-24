6f7b60210898cc391f84bc2abaa477c9cbd5e68f
Corin Chepko <cchepko2@coloradomesa.edu>
2026-08-24 14:08:27 -0600

    Linux Commands

--- Classnotes Fall 26/Linux Commands.cpp
+++ Classnotes Fall 26/Linux Commands.cpp
@@ -2,7 +2,7 @@
 /*
 Author: Corin Chepko
 Date: 8/19/26
-Program Description: Just linux ommands, this is not a c program
+Program Description: Just linux commands, this is not a c program
 */
 
 // First command: ls (stands for list), it lists the contents of a folder
